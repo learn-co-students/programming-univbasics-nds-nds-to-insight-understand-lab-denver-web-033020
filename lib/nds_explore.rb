4394324a94ac11a9e33b131973174497ed8e421a
Lisa Jung <lisahjung@gmail.com>
2020-03-11 16:28:41 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,10 +8,10 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  row_index = 0 
-  while row_index<directors_database[0][:movies].length do
-    puts directors_database[0][:movies][row_index][:title]
-    row_index+=1
+  title_index = 0 
+  while title_index<directors_database[0][:movies].length do
+    puts directors_database[0][:movies][title_index][:title]
+    title_index+=1
   end
 end
 
